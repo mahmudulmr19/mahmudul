4e644f747ed0a1ef5445bdf2e7aa6e5f5ad612d5
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-07-03 00:20:17 +0600

    updated project section

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -1,11 +1,17 @@
 "use client";
 import React from "react";
 import Container from "./Container";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <Container id="projects" className="mt-40">
-      Projects
+      <h2 className="text-white font-semibold text-2xl md:text-3xl lg:text-4xl">
+        All Creative Works.
+      </h2>
+      <p className="text-[#8f9094]">
+        Here&apos;s some of my projects that I have worked on.
+      </p>
     </Container>
   );
 };
